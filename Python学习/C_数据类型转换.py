7396b1fe0ef8b641d1bf2013c97672203c1958b4
chaoyangfeng <1090098083@qq.com>
2026-03-12 10:50:21 +0800

    init

---
+++ Python学习/C_数据类型转换.py
@@ -0,0 +1,30 @@
+'''
+Author: chaoyangfeng 1090098083@qq.com
+Date: 2026-03-12 10:35:32
+LastEditors: chaoyangfeng 1090098083@qq.com
+LastEditTime: 2026-03-12 10:48:54
+FilePath: \Personal_Learning\Python学习\C_数据类型转换.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+'''
+"""
+===========================数据类型转换============================
+数据类型转换：
+数据类型转换是指将一种数据类型转换为另一种数据类型。
+数据类型转换有两种方式：
+1. 强制类型转换：将一种数据类型转换为另一种数据类型，需要使用强制类型转换符号。
+2. 自动类型转换：Python会自动将某些数据类型转换为另一种数据类型。
+
+Python支持以下数据类型转换：
+
+隐式类型转换：
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+"""
