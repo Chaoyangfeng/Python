a79757181dd64ae4b25448e8ad3046b6b6e42e99
chaoyangfeng <1090098083@qq.com>
2026-03-12 16:45:17 +0800

    init

---
+++ Python学习/D_解释器&注释.py
@@ -0,0 +1,25 @@
+'''
+解释器：
+Python解释器是一种程序，它读取并执行Python代码。Python解释器可以直接运行.py文件，也可以在交互模式下运行，也可以作为脚本来运行。
+
+注释：
+Python中单行注释以井号开头，多行注释以三个双引号开头和三个双引号结尾。
+
+单行注释：
+
+```python
+# This is a single line comment
+```
+
+多行注释：
+
+```python
+"""
+This is a multi-line comment
+written in
+more than just one line
+"""
+```
+
+
+'''
